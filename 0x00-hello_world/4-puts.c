913ba0f4712042cb0f9aa70b867bab88c295388b
Ajagunolami <ajagunolami@gmail.com>
2022-12-08 09:32:40 -0800

    4-put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 /**
  *main - print the sentence without using a printf
+ *
  *return - print zero in the end  
 */
 int main(void)
@@ -9,5 +10,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-  
 }

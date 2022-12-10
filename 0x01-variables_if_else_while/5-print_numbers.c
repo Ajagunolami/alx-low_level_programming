98bba035ff709f8cf8e78c81570da546f8acd2f7
Ajagunolami <ajagunolami@gmail.com>
2022-12-09 21:42:58 -0800

    base 10 print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main- print all number of base
+ *Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 9)
+	{
+		printf("%i",x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}

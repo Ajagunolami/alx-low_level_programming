4e8d8e222c1a08c8c768e8dbc524ed5954c063ee
Ajagunolami <ajagunolami@gmail.com>
2022-12-13 10:52:52 -0800

    Exercise 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int _islower - use this code and
+ * look for lower case
+ * Return: 1 if c is lower case
+ */
+int _izlower(int x) 
+{
+	return (x >= 'a' && x <= 'z');
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main- check the code
+ * Description: print_alphabet is used declear function
+ * Return: 1
+ */
+int main(void)
+{
+	int _islower();
+	return (1);
+}

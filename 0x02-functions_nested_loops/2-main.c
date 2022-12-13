a659b309103e34d11446159d7c6434bdbb6c32da
Ajagunolami <ajagunolami@gmail.com>
2022-12-13 10:31:41 -0800

    Excercise 2

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main- check the code
+ * Description: print_alphabet is used declear function
+ * Return: 0 
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}

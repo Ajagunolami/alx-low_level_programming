aee558854ee1062a26bd4ceeb62e5a1708beb6f0
Ajagunolami <ajagunolami@gmail.com>
2022-12-13 18:53:03 -0800

    project

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * @x: the character to be checked
+ * Return: 1 if x is a letter, 0 otherwise
  */
 int _isalpha(int x)
 {

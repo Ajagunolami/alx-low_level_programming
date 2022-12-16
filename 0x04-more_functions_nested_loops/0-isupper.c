fd0ff76a77d63dd66cf55fdc8916591421cc9eae
Ajagunolami <ajagunolami@gmail.com>
2022-12-16 06:25:21 -0800

    check upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper- check for upper case
+ * @c: the caharacter to be checked 
+ * Return: 1  if c is upper case
+ */
+int _isupper(int c)
+{
+	return ((c >= 'A') && (c <= 'Z'));
+}

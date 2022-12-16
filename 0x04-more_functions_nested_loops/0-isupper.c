dab154e19bfdf0081d8a351a545725cde71a49a8
Ajagunolami <ajagunolami@gmail.com>
2022-12-16 06:40:02 -0800

    check for digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper- check for upper case
+ * _isupper - check for uppercase
  * @c: the caharacter to be checked 
- * Return: 1  if c is upper case
+ * Return: 1  if c is uppercase
  */
 int _isupper(int c)
 {
